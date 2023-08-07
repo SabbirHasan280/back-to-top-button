56ced2b4c02e439eb6d2721be9e5cd597dbb0938
Sabbir Hasan Asad <117185573+SabbirHasan280@users.noreply.github.com>
2023-08-07 21:19:16 +0600

    back to top button bootstrap

---
+++ back-to-top/js/custom.js
@@ -0,0 +1,13 @@
+
+$(function(){
+    
+//back-to-top part start//
+
+    $('.back-to-top').click(function(){
+      $('html,body').animate({scrollTop:0},1500) 
+   });
+    
+//back-to-top part end//
+     
+});
+
